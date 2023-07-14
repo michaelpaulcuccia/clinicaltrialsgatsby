6ec58ffc9c4068cdb18cd7072feebeda6339ce2e
Michael Cuccia <michael.cuccia@egplusww.com>
2023-07-14 14:22:22 -0400

    adds form logic

--- client/src/components/StudiesByConditionForm.jsx
+++ client/src/components/StudiesByConditionForm.jsx
@@ -5,16 +5,25 @@ import { getStudiesByCondition } from '../lib/dataFetching';
 export default function StudiesByConditionForm() {
 
     const [conditionStudies, setConditionStudies] = useState([]);
+    const [data, setData] = useState('');
     const [showStudies, setShowStudies] = useState(false);
 
     const handleClick =  async () => {
-        const res = await getStudiesByCondition('acl tear');
+
+      if (data !== '') {
+        const res = await getStudiesByCondition(data);
         setConditionStudies(res);
         setShowStudies(!showStudies);
+        //CLEARS FROM
+        setData('')
+      }
+        
     }
 
   return (
     <Container>
+      <label>Enter Search Criteria</label>
+      <input type='text' value={data} onChange={(event) => setData(event.target.value)} />
       <button onClick={handleClick}>click to see studies</button>
       {
         showStudies &&
