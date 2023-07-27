82748dfbaf94c8a9923d912f9fe63a1891f28a19
Michael Cuccia <michael.cuccia@egplusww.com>
2023-07-27 10:33:28 -0400

    adds hamburger

--- client/src/layout/Nav.jsx
+++ client/src/layout/Nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { breakpoints } from '../constants'
+import { breakpoints } from '../constants';
+import burgerImage from '../../public/assets/burger-menu.svg';
 
 const Root = styled.div`
     background-color: #FAF9F6;
@@ -9,11 +10,31 @@ const Root = styled.div`
     align-items: center;
     justify-content: space-between;
 
-    div {
+    .navItems {
+        -webkit-font-smoothing: antialiased;
+        line-height: 1.25rem;
+        font-size: 14px;
+    }
 
-        &:not(:first-child) {
+    .burgerContainer {
+        display: none;
+        max-width: 70px;
+        padding-right: 10px;
+       
+        @media (max-width: ${breakpoints.navBarMobileBreak}) {
+            display: block;
+            img {
+                height: 32px;
+                width: 32px;
+
+            }
+        }        
+    }
+
+    div {
+        &:not(:first-child):not(:last-child) {
             text-align: center;
-            //HIDES ALL DESKTOP NAV ITEMS EXCEPT FOR LEFT NAV ITEM
+            //HIDES ALL DESKTOP NAV ITEMS EXCEPT FOR LEFT NAV ITEM & HAMBURGER
             @media (max-width: ${breakpoints.navBarMobileBreak}) {
                 display: none;
             }
@@ -21,7 +42,7 @@ const Root = styled.div`
         &:first-child {
             padding-left: 5px;
         }
-        &:last-child {
+        &:nth-last-child(2) {
             padding-right: 5px;
         }       
     }
@@ -30,16 +51,29 @@ const Root = styled.div`
 
 const LeftNavItem = styled.div`
     min-width: 250px;
+
+    .top {
+        font-style: oblique;
+        letter-spacing: 6px;
+        font-size: 18px;
+    }
+
+    .bottom {
+        font-weight: 400;
+    }
 `;
 
 export default function Nav() {
   return (
     <Root>
-        <LeftNavItem>Antigrafo<br/>Clinical Trials</LeftNavItem>
-        <div>What Are <br/>Clinical Trials</div>
-        <div>Why <br/>Participate</div>
-        <div>Oncology <br/>Research</div>
-        <div>Rare Disease <br/>Research</div>
+        <LeftNavItem><span className='top'>Antigrafo</span><br/><span className='bottom'>Clinical Trials</span></LeftNavItem>
+        <div className='navItems'>What Are <br/>Clinical Trials</div>
+        <div className='navItems'>Why <br/>Participate</div>
+        <div className='navItems'>Oncology <br/>Research</div>
+        <div className='navItems'>Rare Disease <br/>Research</div>
+        <div className='burgerContainer'>
+            <img src={burgerImage} alt='' />
+        </div>
     </Root>
   )
 }
